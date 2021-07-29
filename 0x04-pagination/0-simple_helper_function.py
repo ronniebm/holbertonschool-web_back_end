3bb26f6d70a6b76a7c245ff5fb1ab28513fa4a12
ronniebm <ronnie.coding@gmail.com>
2021-07-28 21:48:51 -0500

    fixing

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-""" Pagination """
-
+""" Simple helper function
+"""
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ returns tuple of size two containing a start/end index """
-    pass
+    """ return a tuple of size two containing a start index
+        and an end index corresponding to the range of indexes
+    """
+    if page and page_size:
+        start_index: int = (page - 1) * page_size
+        end_index: int = start_index + page_size
+    return (start_index, end_index)
