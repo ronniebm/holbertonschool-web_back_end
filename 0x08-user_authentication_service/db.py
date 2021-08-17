aeb17b5d3f93629086bcda117c4e5aab16d3ab9a
ronniebm <ronnie.coding@gmail.com>
2021-08-17 13:42:46 -0500

    updating libraries

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,12 +1,13 @@
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-
 from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
 from user import Base, User
+
 
 class DB:
     """DB class
