d5910458b0e59d208f18ac0d36f9dee93b8e9257
ronniebm <ronnie.coding@gmail.com>
2021-08-17 13:40:55 -0500

    fixing missing libraries

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,12 +1,12 @@
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import Base, User
 
 class DB:
     """DB class
