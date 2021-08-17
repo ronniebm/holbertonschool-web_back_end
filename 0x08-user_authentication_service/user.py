ce0ba1f9f6b481ca3c8bd743595c3e3c11632554
ronniebm <ronnie.coding@gmail.com>
2021-08-17 09:18:44 -0500

    trying implementing representative method, which is optional but let's implemented

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -16,3 +16,8 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        return "< User(email='%s', hashed_password='%s', \
+            session_id='%s', reset_token='%s') > " % \
+            (self.email, self.hashed_password, self.session_id)
