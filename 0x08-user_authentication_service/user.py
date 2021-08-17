86fdbbafe1f5a4fe25f17ddd90541b02770a0d58
ronniebm <ronnie.coding@gmail.com>
2021-08-17 09:21:51 -0500

    adding missing class documentation

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -9,6 +9,7 @@ Base = declarative_base()
 
 
 class User(Base):
+    """ SQLAlchemy model for a database table named: users """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
@@ -16,8 +17,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __repr__(self):
-        return "< User(email='%s', hashed_password='%s', \
-            session_id='%s', reset_token='%s') > " % \
-            (self.email, self.hashed_password, self.session_id)
